6f1dc3e3482ae0a1e346ad153a04de432cca0d38
Tetsuo Koyama <tkoyama010@gmail.com>
2019-04-27 20:42:23 +0900

    Update for #1

--- conf.py
+++ conf.py
@@ -21,3 +21,23 @@ execfile_(os.path.join(BASEDIR, 'conf.py'), globals())
 locale_dirs = [os.path.join(BASEDIR, '../../../../locale/')]
 gettext_compact = False
 
+def setup(app):
+    from sphinx.ext.autodoc import cut_lines
+    from sphinx.util.docfields import GroupedField
+    app.srcdir = BASEDIR
+    app.confdir = app.srcdir
+    app.connect('autodoc-process-docstring', cut_lines(4, what=['module']))
+    app.add_object_type('confval', 'confval',
+                        objname='configuration value',
+                        indextemplate='pair: %s; configuration value')
+    fdesc = GroupedField('parameter', label='Parameters',
+                         names=['param'], can_collapse=True)
+    app.add_object_type('event', 'event', 'pair: %s; event', parse_event,
+                        doc_field_types=[fdesc])
+
+    # workaround for RTD
+    from sphinx.util import logging
+    logger = logging.getLogger(__name__)
+    app.info = lambda *args, **kwargs: logger.info(*args, **kwargs)
+    app.warn = lambda *args, **kwargs: logger.warning(*args, **kwargs)
+    app.debug = lambda *args, **kwargs: logger.debug(*args, **kwargs)
