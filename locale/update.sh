420c7ff9231b40d7420f1d524480c28ce3157a95
Tetsuo Koyama <tkoyama010@gmail.com>
2019-04-27 12:52:52 +0900

    [skip ci] update po(t) files

--- locale/update.sh
+++ locale/update.sh
@@ -4,19 +4,19 @@
 set -ex
 
 # required environment variables
-# SPHINXINTL_TRANSIFEX_USERNAME=sphinxjp
-# SPHINXINTL_TRANSIFEX_PROJECT_NAME=sphinx-doc
+export SPHINXINTL_TRANSIFEX_USERNAME=tkoyama010
+export SPHINXINTL_TRANSIFEX_PROJECT_NAME=mayavi-doc
 
 
 # pull po files from transifex
 cd `dirname $0`
 sphinx-intl create-transifexrc
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-sphinx-build -T -b gettext ../sphinx/doc pot
+sphinx-build -T -b gettext ../mayavi/docs/source/mayavi pot
 sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
 tx push -s --skip
-rm -R es ja pt_BR
-tx pull -l es,ja,pt_BR
+rm -Rf ja
+tx pull -l ja
 git checkout .tx/config
 
