31b089c38712119f216679a518dabd3731d8388d
Tetsuo Koyama <tkoyama010@gmail.com>
2020-05-07 20:17:37 +0900

    :recycle: main.yml (#42)
    * :recycle: main.yml
    
    * :recycle: update.sh

--- locale/update.sh
+++ locale/update.sh
@@ -6,16 +6,16 @@ set -ex
 # required environment variables
 # SPHINXINTL_TRANSIFEX_USERNAME=tkoyama010
 # SPHINXINTL_TRANSIFEX_PROJECT_NAME=mayavi
-
+LOCAL_PYTHON_PATH="/home/runner/.local/bin"
 
 # pull po files from transifex
 cd `dirname $0`
-sphinx-intl create-transifexrc
+$LOCAL_PYTHON_PATH/sphinx-intl create-transifexrc
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-sphinx-build -T -b gettext ../mayavi/docs/source/mayavi pot
-sphinx-intl update-txconfig-resources -p pot -d .
+$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../mayavi/docs/source/mayavi pot
+$LOCAL_PYTHON_PATH/sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
-tx push -s --skip
+$LOCAL_PYTHON_PATH/tx push -s --skip
 rm -Rf ja
 tx pull -l ja
 git checkout .tx/config
