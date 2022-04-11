e2a488cf9f7aef015abf73a809cc697b4c85fa0a
Tetsuo Koyama <tkoyama010@gmail.com>
2022-04-11 21:53:01 +0900

    🌇 Sunset of Python3.6 (#57)
    * 🌇 Sunset of Python3.6
    
    * Update main.yml
    
    * Update update.sh

--- locale/update.sh
+++ locale/update.sh
@@ -6,17 +6,17 @@ set -ex
 # required environment variables
 # SPHINXINTL_TRANSIFEX_USERNAME=tkoyama010
 # SPHINXINTL_TRANSIFEX_PROJECT_NAME=mayavi
-LOCAL_PYTHON_PATH="/home/runner/.local/bin"
+# LOCAL_PYTHON_PATH="/home/runner/.local/bin"
 
 # pull po files from transifex
 cd `dirname $0`
-$LOCAL_PYTHON_PATH/sphinx-intl create-transifexrc
+sphinx-intl create-transifexrc
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../mayavi/docs/source/mayavi pot
-$LOCAL_PYTHON_PATH/sphinx-intl update-txconfig-resources -p pot -d .
+sphinx-build -T -b gettext ../mayavi/docs/source/mayavi pot
+sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
-$LOCAL_PYTHON_PATH/tx push -s --skip
+tx push -s --skip
 rm -Rf ja zh_CN
-$LOCAL_PYTHON_PATH/tx pull -l ja,zh_CN
+tx pull -l ja,zh_CN
 git checkout .tx/config
 
